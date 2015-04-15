78642201253ab4b6f78f8319ac82d92d50a67e7f
Elliot Jordan <elliot@elliotjordan.com>
2015-04-14 21:26:38 -0700

    Whitespace and backtick cleanup.

--- Library/Application Support/JAMF/Partners/Scripts/ageCheck.sh
+++ Library/Application Support/JAMF/Partners/Scripts/ageCheck.sh
@@ -15,7 +15,7 @@ daysToGiveNotice=( 5184000 6480000 6912000 7344000 7430400 7516800 7603200 76896
 #Dont change below this line
 ################################################################
 
-currentUser=`whoami | xargs echo -n`   #remove newline char from currentUser
+currentUser=$(whoami | xargs echo -n)   #remove newline char from currentUser
 
 if [ "$currentUser" = "root" ]; then
     echo "This tool cannot run as root.  Execute as admin or standard user."
@@ -23,11 +23,11 @@ if [ "$currentUser" = "root" ]; then
 fi
 
 #get last change of password date
-OSVers=`sysctl -n kern.osrelease | cut -d . -f 1`
-if [ $OSVers -gt 11 ]; then   #for os 10.8+ 
-    lastChangePW=`dscl . -read "/Users/$currentUser" PasswordPolicyOptions |grep passwordLastSetTime -A1 |tail -1|awk -F T '{print $1}'|awk -F \> '{print $2}'`
+OSVers=$(sysctl -n kern.osrelease | cut -d . -f 1)
+if [ $OSVers -gt 11 ]; then   #for os 10.8+
+    lastChangePW=$(dscl . -read "/Users/$currentUser" PasswordPolicyOptions |grep passwordLastSetTime -A1 |tail -1|awk -F T '{print $1}'|awk -F \> '{print $2}')
 elif [ $OSVers -eq 11 ]; then     #for os 10.7
-    lastChangePW=`dscl . -read "/Users/$currentUser" PasswordPolicyOptions |grep passwordTimestamp -A1 |tail -1|awk -F T '{print $1}'|awk -F \> '{print $2}'`
+    lastChangePW=$(dscl . -read "/Users/$currentUser" PasswordPolicyOptions |grep passwordTimestamp -A1 |tail -1|awk -F T '{print $1}'|awk -F \> '{print $2}')
 else
     echo "Unsupported OS Version"
     exit
@@ -40,10 +40,10 @@ if [ -f /Library/Application\ Support/JAMF/Partners/PEAS-Notifier.app/Contents/M
 fi
 
 #get current date and convert it to seconds
-DateNow=`date +%Y-%m-%d`
-DateNowSecs=`date -j -f "%Y-%m-%d" $DateNow "+%s"`
+DateNow=$(date +%Y-%m-%d)
+DateNowSecs=$(date -j -f "%Y-%m-%d" $DateNow "+%s")
 
-userID=`id -u "$currentUser"`  #for local accounts only
+userID=$(id -u "$currentUser")  #for local accounts only
 if [ "$userID" -lt "500" ] || [ "$userID" -gt 1000 ] ;then
     echo "This tool is for local accounts only."
     exit
@@ -53,21 +53,21 @@ fi
 #If no last change date available (new account)
 if [ -z "$lastChangePW" ];then
     echo "$currentUser has no lastChangePW date"
-    
+
 	/usr/bin/osascript <<-EOF3
-           tell application "System Events" 
+           tell application "System Events"
            activate
            display dialog "Partners Warning: The password for account $currentUser needs to be changed. You must change it immediately using System Preferences, 'Users & Groups' button." buttons "OK" default button 1 with icon 2
            end tell
            tell application "System Preferences"
              activate
-             set the current pane to pane id "com.apple.preferences.users"             
+             set the current pane to pane id "com.apple.preferences.users"
            end tell
 EOF3
 else   #checking the age of password here againts limits
-    lastChangePWSeconds=`date -j -f "%Y-%m-%d" $lastChangePW "+%s"`
+    lastChangePWSeconds=$(date -j -f "%Y-%m-%d" $lastChangePW "+%s")
     secondsSinceChanged=$(( $DateNowSecs - $lastChangePWSeconds))
-    daysSinceChanged=$(( $secondsSinceChanged/60/60/24 ))	
+    daysSinceChanged=$(( $secondsSinceChanged/60/60/24 ))
     daysOverExpired=$((90-$daysSinceChanged))
      for dayToCheck in "${daysToGiveNotice[@]}"
      do
@@ -88,7 +88,7 @@ else   #checking the age of password here againts limits
               end if
 EOF
 	else
-	    `"$termNotifierPath"  -title "Your password will expire soon" -activate "com.apple.systempreferences" -message "Your Mac’s local password will expire in $daysTillExpire days. Please change your local password at your earliest convenience."`
+	    "$termNotifierPath"  -title "Your password will expire soon" -activate "com.apple.systempreferences" -message "Your Mac’s local password will expire in $daysTillExpire days. Please change your local password at your earliest convenience."
 	fi
 	   fi
       done
@@ -97,7 +97,7 @@ EOF
 	    /usr/bin/osascript <<-EOF2
               tell application "System Events"
               activate
-              display dialog "Partners Warning: The password for account $currentUser is $daysSinceChanged days old and has expired. You must change it immediately using System Preferences, 'Users & Groups' button." buttons "OK" default button 1 with icon 2 
+              display dialog "Partners Warning: The password for account $currentUser is $daysSinceChanged days old and has expired. You must change it immediately using System Preferences, 'Users & Groups' button." buttons "OK" default button 1 with icon 2
               end tell
               tell application "System Preferences"
                       activate
@@ -108,7 +108,7 @@ EOF2
               tell application "System Preferences"
                       activate
                       set the current pane to pane id "com.apple.preferences.users"
-                 end tell 
+                 end tell
 EOF4
     fi
 fi
--- Library/Application Support/JAMF/Partners/Scripts/pwpolicyPerUser.sh
+++ Library/Application Support/JAMF/Partners/Scripts/pwpolicyPerUser.sh
@@ -5,7 +5,7 @@
 # to deploy pwpolicy to the currently logged in user, not all users or global 'cause that screws up other stuff
 
 #get list of users who are logged in
-currentLoggedInUsers=`w -h | grep console| sort -u -t' ' -k1,1|awk '{print $1}'`
+currentLoggedInUsers=$(w -h | grep console| sort -u -t' ' -k1,1|awk '{print $1}')
 
 currentLoggedInUsersCR="$currentLoggedInUsers
 " #to add CR
@@ -14,26 +14,26 @@ currentLoggedInUsersCR="$currentLoggedInUsers
 printf %s "$currentLoggedInUsersCR" | while IFS=$'\n' read -r currentLoggedInUser
 #for currentLoggedInUser in $currentLoggedInUsersCR
 do
-    
+
     echo "begining run on $currentLoggedInUser user"
     if [ "$currentLoggedInUser" == "root" ] ||  [ "$currentLoggedInUser" == "PHS Admin" ] ||  [ "$currentLoggedInUser" == "daemon" ]; then
 	exit
     fi
-    
+
 #create user storage dir if not exists
     if [[ ! -d /Library/Application\ Support/JAMF/Partners/Library/passwordPolicyPerUser ]]; then
         mkdir /Library/Application\ Support/JAMF/Partners/Library
 	mkdir /Library/Application\ Support/JAMF/Partners/Library/passwordPolicyPerUser
 	echo "pwpolicyPerUser.sh will create one file per user so pwpolicy is set only once for each user" > /Library/Application\ Support/JAMF/Partners/Library/passwordPolicyPerUser/readme.txt
-	`pwpolicy -setglobalpolicy "usingHistory=4"`
+	pwpolicy -setglobalpolicy "usingHistory=4"
     fi
-    
+
 #run only once per user
     if [[ -f /Library/Application\ Support/JAMF/Partners/Library/passwordPolicyPerUser/$currentLoggedInUser ]]; then
 	echo "policy exists for $currentLoggedInUser"
     else
 	echo "setting $currentLoggedInUser policy"
-        `pwpolicy -u $currentLoggedInUser -setpolicy "requiresAlpha=1 requiresNumeric=1 minChars=8 usingHistory=4"`
-        `touch /Library/Application\ Support/JAMF/Partners/Library/passwordPolicyPerUser/$currentLoggedInUser`
+        pwpolicy -u $currentLoggedInUser -setpolicy "requiresAlpha=1 requiresNumeric=1 minChars=8 usingHistory=4"
+        touch /Library/Application\ Support/JAMF/Partners/Library/passwordPolicyPerUser/$currentLoggedInUser
     fi
 done
